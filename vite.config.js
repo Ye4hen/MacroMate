32af3323d7af6129d8dd587af3d793d54e4079d9
Ye4hen <bosak.junior.2006@gmail.com>
2025-10-25 13:39:09 +0200

    Fixing ci vite build

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,48 @@
-import {defineConfig} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import legacy from '@vitejs/plugin-legacy';
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import legacy from "@vitejs/plugin-legacy";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({mode}) => {
-    const is_dev = mode === 'development';
+export default defineConfig(({ mode }) => {
+    const is_dev = mode === "development";
 
     return {
         plugins: [
             laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js', 'resources/images/*'],
+                input: [
+                    "resources/css/app.css",
+                    "resources/js/app.js",
+                ],
                 refresh: true,
             }),
             legacy({
-                targets: ['defaults', 'not IE 11'],
-                additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+                targets: ["defaults", "not IE 11"],
+                additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
             }),
             tailwindcss(),
         ],
         resolve: {
             alias: {
-                '@': path.resolve(__dirname, 'resources'),
+                "@": path.resolve(__dirname, "resources"),
             },
         },
         server: {
-            host: '127.0.0.1',
+            host: "127.0.0.1",
             port: 1488,
             strictPort: true,
 
             hmr: {
-                host: '127.0.0.1',
-                protocol: 'ws',
+                host: "127.0.0.1",
+                protocol: "ws",
             },
         },
         build: {
-            outDir: 'public/build',
+            outDir: "public/build",
             emptyOutDir: true,
         },
         css: {
             devSourcemap: is_dev,
         },
-    }
+    };
 });
