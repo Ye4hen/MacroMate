e356dbcca299b420329ec1698152b383a5df663c
Ye4hen <bosak.junior.2006@gmail.com>
2025-11-09 19:14:36 +0100

    Stop tracking phpstan/php-cs-fixer cache files

--- phpstan-cache/cache/PHPStan/0c/4a/0c4a83a1ef717eda8920eb4c0c4ebda5b77f91e5.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types = 1);
-
-// odsl-C:/Users/Yevhen/Desktop/programming_lessons/PHP/Laravel/calories_tracker/vendor/composer/../phar-io/version/src/
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => 'v1',
-   'data' => 
-  array (
-  ),
-));
--- phpstan-cache/cache/PHPStan/37/be/37becbcca109260bc9d57ff7f2512be53098fbd9.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types = 1);
-
-// odsl-C:/Users/Yevhen/Desktop/programming_lessons/PHP/Laravel/calories_tracker/vendor/composer/../sebastian/recursion-context/src/
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => 'v1',
-   'data' => 
-  array (
-  ),
-));
